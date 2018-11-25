371c951ef6eb420f8d42fb906ec68a237e5ffeb3
Luca Pompei <lucapompei91@gmail.com>
2018-11-25 22:57:12 +0100

    generalized "initialize" configurator + check null for env property

--- src/main/java/it/christianlusardi/mcu/core/Configurator.java
+++ src/main/java/it/christianlusardi/mcu/core/Configurator.java
@@ -1,7 +1,5 @@
 package it.christianlusardi.mcu.core;
 
-
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -11,8 +9,6 @@ import org.slf4j.LoggerFactory;
 
 import it.christianlusardi.mcu.constants.GlobalConstants;
 import it.christianlusardi.mcu.exceptions.ConfigurationFileNotFoundException;
-
-
 
 /**
  * Configuratore generale delle proprietà. Questo oggetto consente, una volta
@@ -24,45 +20,38 @@ import it.christianlusardi.mcu.exceptions.ConfigurationFileNotFoundException;
  */
 public class Configurator {
 
+	/**
+	 * The logger
+	 */
 	private static final Logger LOGGER = LoggerFactory.getLogger(Configurator.class);
 
-	protected static String path = "";
-	protected static Properties prop = new Properties();
-	
-	
-	
+	private static Properties prop = new Properties();
+
+	/**
+	 * Private constructor for an utility class
+	 */
 	private Configurator() {
 		throw new IllegalAccessError(GlobalConstants.STANDARD_MESSAGE_UTILITY_CLASS);
 	}
-	
-	
-	
+
 	/**
 	 * Procedura iniziale per inizializzare l'oggetto {@link Configurator}
 	 * 
-	 * @throws ConfigurationFileNotFoundException exception when configuration file was not found
+	 * @throws ConfigurationFileNotFoundException exception when configuration file
+	 *                                            was not found
 	 * 
-	 * @throws IOException exception when there are IO problems
+	 * @throws IOException                        exception when there are IO
+	 *                                            problems
 	 */
 	public static void initialize() throws ConfigurationFileNotFoundException, IOException {
-		
-		String fileName = EnvironmentUtils.getConfigurationFileName(EnvironmentUtils.getEnvironmentProfile());
-		ClassLoader classLoader = Configurator.class.getClassLoader();
-		
-		try (InputStream inputStream = classLoader.getResourceAsStream(fileName)) {
-			// caricamento delle properties
-			prop.load(inputStream);
-		} catch ( NullPointerException e) {
-			LOGGER.error("Configuration file [{}] not found", fileName);
 
-			throw new ConfigurationFileNotFoundException("Configuration file " + fileName + " not found");
-		}
+		String profile = EnvironmentUtils.getEnvironmentProfile();
+		String pathParam = EnvironmentUtils.getConfigurationFileName(profile);
 
-		LOGGER.info("configuration file loaded");
+		initialize(pathParam);
+
 	}
-	
-	
-	
+
 	/**
 	 * Procedura iniziale per inizializzare l'oggetto {@link Configurator}
 	 * 
@@ -70,19 +59,20 @@ public class Configurator {
 	 * 
 	 * @throws IOException IO problems
 	 */
-	public static void initialize(String pathParam) throws IOException {
+	public static void initialize(String pathParam) throws ConfigurationFileNotFoundException, IOException {
+		ClassLoader classLoader = Configurator.class.getClassLoader();
 
-		path = pathParam;
+		try (InputStream inputStream = classLoader.getResourceAsStream(pathParam)) {
+			// caricamento delle properties
+			prop.load(inputStream);
+		} catch (NullPointerException e) {
+			LOGGER.error("Configuration file [{}] not found", pathParam);
+			throw new ConfigurationFileNotFoundException("Configuration file " + pathParam + " not found");
+		}
 
-		// inizializzazione dello stream di input del file delle properties
-		InputStream inputStream = new FileInputStream(path);
-
-		// caricamento delle properties
-		prop.load(inputStream);
+		LOGGER.info("configuration file loaded");
 	}
-	
-	
-	
+
 	/**
 	 * Funzione che ritorna il valore della proprietà indicata
 	 * 
@@ -92,9 +82,7 @@ public class Configurator {
 	public static String getStringValue(String propertyName) {
 		return prop.getProperty(propertyName);
 	}
-	
-	
-	
+
 	/**
 	 * Funzione che ritorna il valore della proprietà indicata. In caso di valore
 	 * nullo o vuoto, verrà restituito il valore indicato di default come parametro.
@@ -106,9 +94,7 @@ public class Configurator {
 	public static String getStringValue(String propertyName, String defaultValue) {
 		return prop.getProperty(propertyName, defaultValue);
 	}
-	
-	
-	
+
 	/**
 	 * Funzione che ritorna il valore della proprietà indicata.
 	 * 
@@ -120,18 +106,18 @@ public class Configurator {
 	 */
 	public static int getIntValue(String propertyName) {
 		int result;
+
 		String strResult = prop.getProperty(propertyName);
 
-		if (strResult.isEmpty())
+		if (strResult == null || strResult.isEmpty()) {
 			throw new NullPointerException("Property " + propertyName + " is empty.");
-		else
+		} else {
 			result = Integer.parseInt(strResult);
+		}
 
 		return result;
 	}
-	
-	
-	
+
 	/**
 	 * Funzione che ritorna il valore della proprietà indicata. In caso di campo non
 	 * valorizzato (un intero non può essere nullo), verrà restituito il valore
@@ -143,18 +129,18 @@ public class Configurator {
 	 */
 	public static int getIntValue(String propertyName, int defaultValue) {
 		int result;
+
 		String strResult = prop.getProperty(propertyName);
 
-		if (strResult.isEmpty())
+		if (strResult == null || strResult.isEmpty()) {
 			result = defaultValue;
-		else
+		} else {
 			result = Integer.parseInt(strResult);
+		}
 
 		return result;
 	}
-	
-	
-	
+
 	/**
 	 * Funzione che ritorna il valore della proprietà indicata.
 	 * 
@@ -166,18 +152,18 @@ public class Configurator {
 	 */
 	public static long getLongValue(String propertyName) {
 		long result;
+
 		String strResult = prop.getProperty(propertyName);
 
-		if (strResult.isEmpty())
+		if (strResult == null || strResult.isEmpty()) {
 			throw new NullPointerException("Property " + propertyName + " is empty.");
-		else
+		} else {
 			result = Long.parseLong(strResult);
+		}
 
 		return result;
 	}
-	
-	
-	
+
 	/**
 	 * Funzione che ritorna il valore della proprietà indicata. In caso di campo non
 	 * valorizzato (un long non può essere nullo), verrà restituito il valore
@@ -189,18 +175,18 @@ public class Configurator {
 	 */
 	public static long getLongValue(String propertyName, long defaultValue) {
 		long result;
+
 		String strResult = prop.getProperty(propertyName);
 
-		if (strResult.isEmpty())
+		if (strResult == null || strResult.isEmpty()) {
 			result = defaultValue;
-		else
+		} else {
 			result = Long.parseLong(strResult);
+		}
 
 		return result;
 	}
-	
-	
-	
+
 	/**
 	 * Funzione che ritorna il valore della proprietà indicata.
 	 * 
@@ -212,18 +198,18 @@ public class Configurator {
 	 */
 	public static float getFloatValue(String propertyName) {
 		float result;
+
 		String strResult = prop.getProperty(propertyName);
 
-		if (strResult.isEmpty())
+		if (strResult == null || strResult.isEmpty()) {
 			throw new NullPointerException("Property " + propertyName + " is empty.");
-		else
+		} else {
 			result = Float.parseFloat(strResult);
+		}
 
 		return result;
 	}
-	
-	
-	
+
 	/**
 	 * Funzione che ritorna il valore della proprietà indicata. In caso di campo non
 	 * valorizzato (un float non può essere nullo), verrà restituito il valore
@@ -235,18 +221,18 @@ public class Configurator {
 	 */
 	public static float getFloatValue(String propertyName, float defaultValue) {
 		float result;
+
 		String strResult = prop.getProperty(propertyName);
 
-		if (strResult.isEmpty())
+		if (strResult == null || strResult.isEmpty()) {
 			result = defaultValue;
-		else
+		} else {
 			result = Float.parseFloat(strResult);
+		}
 
 		return result;
 	}
-	
-	
-	
+
 	/**
 	 * Funzione che ritorna il valore della proprietà indicata.
 	 * 
@@ -256,9 +242,10 @@ public class Configurator {
 	 */
 	public static double getDoubleValue(String propertyName) {
 		double result;
+
 		String strResult = prop.getProperty(propertyName);
 
-		if (strResult.isEmpty()) {
+		if (strResult == null || strResult.isEmpty()) {
 			throw new NullPointerException("Property " + propertyName + " is empty.");
 		} else {
 			result = Double.parseDouble(strResult.replace(',', '.'));
@@ -266,9 +253,7 @@ public class Configurator {
 
 		return result;
 	}
-	
-	
-	
+
 	/**
 	 * Funzione che ritorna il valore della proprietà indicata. In caso di campo non
 	 * valorizzato (un double non può essere nullo), verrà restituito il valore
@@ -280,14 +265,17 @@ public class Configurator {
 	 */
 	public static double getDoubleValue(String propertyName, double defaultValue) {
 		double result;
+
 		String strResult = prop.getProperty(propertyName);
 
-		if (strResult.isEmpty())
+		if (strResult == null || strResult.isEmpty()) {
 			result = defaultValue;
-		else
+		} else {
 			result = Double.parseDouble(strResult);
+		}
 
 		return result;
+
 	}
 
 }
